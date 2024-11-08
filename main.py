2e4eb93d93be9ab5f6f3df19a382e537ccba121e
carl1235 <carl.nakad@gmail.com>
2024-11-08 01:51:40 -0500

    Reduce batch_size, refactor CNN and add MLP model

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import dataset
 from models.gaussian_naive_bayes import GaussianNaiveBayes
 from models.decision_tree import DecisionTree
 from models.convolution_nn import ConvolutionNeuralNetwork
+from models.mlp import MultiLayerPerceptron
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score
 import pickle
@@ -27,7 +28,7 @@ def load_or_cache_data(cache_path="./data/cached_data.pkl", batch_size=128, devi
 if __name__ == '__main__':
     # Load or cache the data
     device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available()  else "cpu")
-    train_loader, test_loader, train_feature, train_labels, test_features, test_labels = load_or_cache_data(batch_size=64, device=device)
+    train_loader, test_loader, train_feature, train_labels, test_features, test_labels = load_or_cache_data(batch_size=32, device=device)
     
     # Train and evaluate using my Gaussian Naive Bayes implementation
     model = GaussianNaiveBayes()
@@ -44,9 +45,22 @@ if __name__ == '__main__':
     # model = DecisionTree(50)
     # test_pred = model.fit(train_feature, train_labels)
 
-    model = ConvolutionNeuralNetwork(device=device)
-    test_pred = model.fit(train_loader).predict(test_features)
+    model = MultiLayerPerceptron(device=device, num_epochs=1)
+    if os.path.exists('bestMLP_model.pth'):
+        model.load_state_dict(torch.load('bestMLP_model.pth', weights_only=True))
+    else:
+        model.fit(train_feature, train_labels)
+    test_pred = model.predict(train_feature, train_labels)
 
+    accuracy = accuracy_score(test_labels, test_pred)
+    print(f"SK implementation accuracy: {accuracy * 100:.2f}%")
+
+    model = ConvolutionNeuralNetwork(device=device, num_epochs=1)
+    if os.path.exists('bestCNN_model.pth'):
+        model.load_state_dict(torch.load('bestCNN_model.pth', weights_only=True))
+    else:
+        model.fit(train_loader)
+    test_pred = model.predict(test_loader)
     
     accuracy = accuracy_score(test_labels, test_pred)
     print(f"SK implementation accuracy: {accuracy * 100:.2f}%")
