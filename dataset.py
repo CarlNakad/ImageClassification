d644ecb9ba7bd0c51857da7d569858a6b082eefc
carl1235 <carl.nakad@gmail.com>
2024-11-19 15:40:39 -0500

    Add load or cache data from main Add transform parameter to load_cifar10

--- dataset.py
+++ dataset.py
@@ -2,8 +2,9 @@ from sklearn.decomposition import PCA
 import torch
 import torchvision
 import torchvision.transforms as transforms
-import matplotlib.pyplot as plt
-import numpy as np
+import os
+import pickle
+
 
 def filter_dataset(dataset, total_samples):
     num_classes = 10
@@ -17,14 +18,7 @@ def filter_dataset(dataset, total_samples):
                 break
     return torch.utils.data.Subset(dataset, indices)
 
-def load_cifar10(batch_size):
-    # Resize and Normalize for ResNet-18
-    transform = transforms.Compose([
-        transforms.Resize((224, 224)),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-    ])
-
+def load_cifar10(transform, batch_size):
     # Load the full CIFAR-10 dataset
     full_train_set = torchvision.datasets.CIFAR10(root='./data', train=True,
                                                  download=True, transform=transform)
@@ -59,9 +53,16 @@ def extract_features(data_loader, model, device):
     return features, labels
 
 def load_data(batch_size, device):
+    # Resize and Normalize for ResNet-18
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+
     print(f"Using device: {device}")
     print("Loading CIFAR-10 dataset...")
-    train_loader, test_loader = load_cifar10(batch_size)
+    train_loader, test_loader = load_cifar10(transform, batch_size)
 
     print("ResNet-18 model as a feature extractor...")
     resnet18 = torchvision.models.resnet18(weights=torchvision.models.ResNet18_Weights.DEFAULT)
@@ -78,4 +79,22 @@ def load_data(batch_size, device):
     train_features = pca.fit_transform(train_features)
     test_features = pca.transform(test_features)
 
-    return train_loader, test_loader, train_features, train_labels, test_features, test_labels
+    return train_features, train_labels, test_features, test_labels
+
+
+def load_or_cache_data(cache_path="./data/cached_data.pkl", batch_size=128, device=torch.device("cpu")):
+    # Check if cached data exists
+    if os.path.exists(cache_path):
+        print("Loading data from cache...")
+        with open(cache_path, "rb") as f:
+            train_feature, train_labels, test_features, test_labels = pickle.load(f)
+    else:
+        print("Loading data from dataset and caching it...")
+        # Load data if cache doesn't exist
+        train_feature, train_labels, test_features, test_labels = load_data(batch_size=batch_size,
+                                                                                    device=device)
+        # Cache the data for future use
+        with open(cache_path, "wb") as f:
+            pickle.dump((train_feature, train_labels, test_features, test_labels), f)
+
+    return train_feature, train_labels, test_features, test_labels
