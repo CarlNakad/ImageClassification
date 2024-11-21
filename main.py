a6807f733f89ac40e45d6e2426f9e0ed7c00e759
carl1235 <carl.nakad@gmail.com>
2024-11-20 22:36:24 -0500

    Moved all models to run from model_runner.py and added arguments

--- main.py
+++ main.py
@@ -1,97 +1,100 @@
-import pickle
-
-from dataset import load_or_cache_data, load_cifar10
-from evaluate_models import evaluate_model
-from models.gaussian_naive_bayes import GaussianNaiveBayes
-from models.decision_tree import DecisionTree
-from models.convolution_nn import ConvolutionNeuralNetwork
-from models.mlp import MultiLayerPerceptron
-from sklearn.naive_bayes import GaussianNB
-from sklearn.metrics import accuracy_score, confusion_matrix
-from sklearn.tree import DecisionTreeClassifier
-import torchvision.transforms as transforms
 import os
+from datetime import datetime
+from dataset import load_or_cache_data
+from evaluate_models import evaluate_model, generate_comparison_table
+from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
 import torch
-model_data_path = "./data/model_data"
+import argparse
+from model_runner import ModelRunner
+
+MODEL_DATA_PATH = "./data/model_data"
 
 if __name__ == '__main__':
+    # Create a folder to save the figures
+    date_string = datetime.now().strftime("%d-%m-%Y %H:%M")
+    FIGURE_PATH = f'./figures/{date_string}'
+    print(f"Saving figures to {FIGURE_PATH}")
+    os.makedirs(FIGURE_PATH, exist_ok=True)
+
+    # Create a folder to save the model data
+    os.makedirs(MODEL_DATA_PATH, exist_ok=True)
+
     # Set the device to GPU if available (mps is used for Apple Silicon processors)
     device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available()  else "cpu")
+
     # Load or cache the data
     train_feature, train_labels, test_features, test_labels = load_or_cache_data(batch_size=32, device=device)
     classes = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
-    
-    # # Train and evaluate using my Gaussian Naive Bayes implementation
-    # model = GaussianNaiveBayes()
-    # test_pred = model.fit(train_feature, train_labels).predict(test_features)
-    # evaluate_model("My GaussianNB model", test_labels, test_pred, classes)
-    #
-    # # Train and evaluate using sklearn's Gaussian Naive Bayes implementation
-    # model = GaussianNB()
-    # test_pred = model.fit(train_feature, train_labels).predict(test_features)
-    # evaluate_model("SK GaussianNB model", test_labels, test_pred, classes)
-    #
-    # # Train and evaluate using my Decision Tree implementation with different depths
-    # decision_tree_depths = [1, 5, 10, 15, 50]
-    # for depth in decision_tree_depths:
-    #     model = DecisionTree(depth, file_path=f"{model_data_path}/decision_tree_model_{depth}.pkl")
-    #     test_pred = model.fit(train_feature, train_labels).predict(test_features)
-    #     evaluate_model(f"My Decision Tree model with depth {depth}", test_labels, test_pred, classes)
-    #
-    # # Train and evaluate using sklearn's Decision Tree implementation
-    # model = DecisionTreeClassifier(max_depth=20)
-    # test_pred = model.fit(train_feature, train_labels).predict(test_features)
-    # evaluate_model("SK Decision Tree model",test_labels, test_pred, classes)
-    #
-    # # Train and evaluate using my Multi Layer Perceptron implementation
-    # num_epochs = 10
-    # model = MultiLayerPerceptron(device=device, num_epochs=num_epochs, hidden_layer_size=512, layer_removal=True, file_path=f"{model_data_path}/bestMLP_model_{num_epochs}_removed_layer.pth")
-    # test_pred = (model.fit(train_features=train_feature, train_labels=train_labels)
-    #              .predict(test_features=test_features, test_labels=test_labels))
-    # evaluate_model(f"My MLP model with {num_epochs} epochs and removed layer", test_labels, test_pred, classes)
-    #
-    # model = MultiLayerPerceptron(device=device, num_epochs=num_epochs, hidden_layer_size=512, layer_addition=True, file_path=f"{model_data_path}/bestMLP_model_{num_epochs}_additional_layer.pth")
-    # test_pred = (model.fit(train_features=train_feature, train_labels=train_labels)
-    #              .predict(test_features=test_features, test_labels=test_labels))
-    # evaluate_model(f"My MLP model with {num_epochs} epochs and additional layer", test_labels, test_pred, classes)
-    #
-    #
-    # hidden_layer_sizes = [256, 512, 1024]
-    # # Evaluate the model with different hidden layer sizes
-    # for hidden_layer_size in hidden_layer_sizes:
-    #     model = MultiLayerPerceptron(device=device, num_epochs=num_epochs, hidden_layer_size=hidden_layer_size, file_path=f"{model_data_path}/bestMLP_model_{num_epochs}_{hidden_layer_size}.pth")
-    #     test_pred = (model.fit(train_features=train_feature, train_labels=train_labels)
-    #                  .predict(test_features=test_features, test_labels=test_labels))
-    #     evaluate_model(f"My MLP model with {num_epochs} epochs and hidden layer size {hidden_layer_size}",test_labels, test_pred, classes)
 
-    # Train and evaluate using my Convolution Neural Network implementation
-    # Define the transformations for the images
-    transform = transforms.Compose([
-        transforms.RandomHorizontalFlip(),
-        transforms.RandomCrop(32, padding=4),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-    ])
+    model_runner = ModelRunner(train_feature, train_labels, test_features, test_labels, classes, FIGURE_PATH, device)
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(description="Run the CIFAR-10 project.")
+    parser.add_argument("--model", choices=["naive_bayes", "decision_tree", "mlp", "cnn", "all"], default="all",
+                        help="Specify which model to run.")
+    parser.add_argument("--max-depth", type=int, default=50, help="Specify the max depth for the decision tree model.")
+    parser.add_argument("--epochs", type=int, default=50, help="Specify the number of epochs for the MLP model or CNN model.")
+    parser.add_argument("--hidden-layer-size", type=int, default=512, help="Specify the hidden layer size for the MLP model.")
+    parser.add_argument("--layer-removal", action="store_true", default=False, help="Specify whether to remove a layer from the MLP model or CNN model.")
+    parser.add_argument("--layer-addition", action="store_true", default=False, help="Specify whether to add a layer to the MLP model or CNN model.")
+    parser.add_argument("--kernal-size", type=int, default=3, help="Specify the kernel size for the CNN model.")
+    args = parser.parse_args()
 
-    # Load the CIFAR-10 dataset with the new transforms
-    train_loader, test_loader = load_cifar10(transform, 32)
+    if args.model == "naive_bayes":
+        model_runner.run_naive_bayes_model()
+    elif args.model == "decision_tree":
+        depth = args.max_depth
+        model_runner.run_decision_tree_model(depth)
+    elif args.model == "mlp":
+        num_epochs = args.epochs
+        hidden_layer_size = args.hidden_layer_size
+        layer_removal = args.layer_removal
+        layer_addition = args.layer_addition
+        model_runner.run_mlp_model(num_epochs, hidden_layer_size, layer_removal, layer_addition)
+    elif args.model == "cnn":
+        num_epochs = args.epochs
+        kernel_size = args.kernal_size
+        layer_removal = args.layer_removal
+        layer_addition = args.layer_addition
+        model_runner.run_cnn_model(num_epochs, kernel_size, layer_removal, layer_addition)
+    else:
+        # Train and evaluate using my Gaussian Naive Bayes implementation
+        model_runner.run_naive_bayes_model()
 
-    num_epochs = 200
+        # Train and evaluate using sklearn's Gaussian Naive Bayes implementation
+        model = GaussianNB()
+        test_pred = model.fit(train_feature, train_labels).predict(test_features)
+        model_runner.append_report(evaluate_model("SK GaussianNB model", test_labels, test_pred, classes, FIGURE_PATH))
 
-    # Evaluate the model with different kernel sizes
-    kernel_sizes = [2, 3, 5]
-    for kernel_size in kernel_sizes:
-        model = ConvolutionNeuralNetwork(device=device, num_epochs=num_epochs, kernel_size=kernel_size, file_path=f"{model_data_path}/bestCNN_model_{num_epochs}_{kernel_size}.pth")
-        test_pred = model.fit(dataset=train_loader.dataset).predict(data_loader=test_loader)
-        evaluate_model("My CNN model",test_labels, test_pred, classes)
+        # Train and evaluate using my Decision Tree implementation with different depths
+        decision_tree_depths = [1, 5, 10, 15, 50]
+        for depth in decision_tree_depths:
+            model_runner.run_decision_tree_model(depth)
 
-    # Evaluate the model with layer addition
-    # model = ConvolutionNeuralNetwork(device=device, num_epochs=num_epochs, layer_addition=True, file_path=f"{model_data_path}/bestCNN_model_{num_epochs}_additional_layer.pth")
-    # test_pred = model.fit(dataset=train_loader.dataset).predict(data_loader=test_loader)
-    # evaluate_model("My CNN model with additional layer", test_labels, test_pred, classes)
-    #
-    # # Evaluate the model with layer removal
-    # model = ConvolutionNeuralNetwork(device=device, num_epochs=num_epochs, layer_removal=True, file_path=f"{model_data_path}/bestCNN_model_{num_epochs}_removed_layer.pth")
-    # test_pred = model.fit(dataset=train_loader.dataset).predict(data_loader=test_loader)
-    # evaluate_model("My CNN model with removed layer", test_labels, test_pred, classes)
+        # Train and evaluate using sklearn's Decision Tree implementation
+        model = DecisionTreeClassifier(max_depth=50)
+        test_pred = model.fit(train_feature, train_labels).predict(test_features)
+        model_runner.append_report(evaluate_model("SK Decision Tree model with depth 50",test_labels, test_pred, classes, FIGURE_PATH))
+
+        # Train and evaluate using my Multi Layer Perceptron implementation
+        num_epochs = 50
+        hidden_layer_sizes = [256, 512, 1024]
+        for hidden_layer_size in hidden_layer_sizes:
+            model_runner.run_mlp_model(num_epochs, hidden_layer_size, False, False)
+
+        model_runner.run_mlp_model(num_epochs, 512, True, False)
+        model_runner.run_mlp_model(num_epochs, 512, False, True)
+
+        num_epochs = 100
+        kernel_sizes = [3, 5]
+        for kernel_size in kernel_sizes:
+           model_runner.run_cnn_model(num_epochs, kernel_size, False, False)
+
+        model_runner.run_cnn_model(num_epochs, 3, True, False)
+        model_runner.run_cnn_model(num_epochs, 3, False, True)
+
+    # Create table of models' classification reports
+    print("\nGenerating comparison table...")
+    generate_comparison_table(model_runner.models_report, FIGURE_PATH)
+
 
