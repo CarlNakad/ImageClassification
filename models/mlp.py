59c70c3fc4dcb6457e39d1f94cc8600687bfc12a
carl1235 <carl.nakad@gmail.com>
2024-11-20 22:35:19 -0500

    Add comments to mlp.py

--- models/mlp.py
+++ models/mlp.py
@@ -6,6 +6,17 @@ from models.common.neural_network import NeuralNetwork
 
 class MultiLayerPerceptron(NeuralNetwork):
     def __init__(self, device="cpu", num_epochs=10, hidden_layer_size=512, file_path="bestMLP_model.pth", layer_addition=False, layer_removal=False):
+        """
+        Multi-Layer Perceptron (MLP) with 2 hidden layers.
+        The model can be trained with an additional layer or with a layer removed.
+
+        :param device: Device to run the model on
+        :param num_epochs: Number of epochs to train the model
+        :param hidden_layer_size: Size of the hidden layer
+        :param file_path: File path to save the model
+        :param layer_addition: Whether to add an extra layer to the model
+        :param layer_removal: Whether to remove a layer from the model
+        """
         super().__init__()
         self.num_epochs = num_epochs
         self.device = device
@@ -20,6 +31,12 @@ class MultiLayerPerceptron(NeuralNetwork):
         self.fc3 = nn.Linear(hidden_layer_size, 10)
 
     def forward(self, x):
+        """
+        Forward pass of the model
+
+        :param x: Input tensor
+        :return: Output tensor
+        """
         x = x.view(x.size(0), -1)
         x = F.relu(self.fc1(x))
         if self.layer_removal:
